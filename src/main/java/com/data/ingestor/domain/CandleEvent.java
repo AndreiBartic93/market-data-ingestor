156437d8374ecb09c7ef29eaff6719bcb78ba2a1
AndreiBartic93 <barticandrei22@gmail.com>
2025-12-22 17:05:35 +0200

    Added CandleEvent

--- src/main/java/com/data/ingestor/domain/CandleEvent.java
+++ src/main/java/com/data/ingestor/domain/CandleEvent.java
@@ -3,15 +3,15 @@ package com.data.ingestor.domain;
 import java.math.BigDecimal;
 
 public record CandleEvent(
-        String exchange,
-        String symbol,
-        String interval,
-        long openTime,
-        long closeTime,
+        String exchange,          // "BINANCE"
+        String symbol,            // "ETHUSDT"
+        String interval,          // "1h"
+        long openTime,            // millis
+        long closeTime,           // millis
         BigDecimal open,
         BigDecimal high,
         BigDecimal low,
         BigDecimal close,
         BigDecimal volume,
-        long eventTime
+        long eventTime            // millis (cand publicam)
 ) {}
